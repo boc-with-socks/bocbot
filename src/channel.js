34ecdd8ca88287c962e32e8b345bbd2a6ff76df0
boc-with-socks <albackeh@hotmail.fr>
2019-11-12 22:19:25 +0100

    testttt

--- src/channel.js
+++ src/channel.js
@@ -143,7 +143,7 @@ module.exports = class Channel
                 break
 
             case 15:
-                this.sendMessage('current list of jobless nerds:\n-mercury\n-savi\n-porro\n-fiptesttestagain2')
+                this.sendMessage('current list of jobless nerds:\n-mercury\n-savi\n-porro\n-fip')
                 break
 
             default:
